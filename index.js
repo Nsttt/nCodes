118f13c82bff4f59c3caa6c4b3a9d63c04139843
Nestor <elnestinatorHD@gmail.com>
2020-10-20 23:21:35 +0200

    Working so far. Needs more polish.

--- index.js
+++ index.js
@@ -1,78 +1,75 @@
 const Discord = require("discord.js");
 const NanaAPI = require("nana-api");
 const { DateTime } = require("luxon");
-const Keyv = require('keyv');
 
-const config = require('./secret/config.json');
+const config = require("./secret/config.json");
 
-const globalPrefix = '!n';
 const client = new Discord.Client();
 const nana = new NanaAPI();
 
-const prefixes = new Keyv();
-prefixes.on('error', err => console.error('Keyv connection error:', err));
-
-// const embed = new Discord.MessageEmbed()
-//   .setColor("#ED2553")
-//   .setAuthor(
-//     scanlator,
-//     "./static/icon.png",
-//     "https://nhentai.net/artist/" + scanlator
-//   )
-//   .setThumbnail(images.cover)
-//   .setTitle(title.pretty)
-//   .setURL("https://nhentai.net/g/" + id)
-//   .addFields({ name: "Pages:", value: num_pages, inline: true })
-//   .setTimestamp(DateTime.toMillis(upload_date))
-//   .setFooter("Favorites: " + num_favorites, "./static/heart-solid.svg");
-
-const newSearch = (i) => {
-  nana.g(i).then((g) => {
-
-  })
-}
-
 // Client setup
-client.on('message', async message => {
-	if (message.author.bot) return;
 
-	let args;
-	// handle messages in a guild
-	if (message.guild) {
-		let prefix;
-
-		if (message.content.startsWith(globalPrefix)) {
-			prefix = globalPrefix;
-		} else {
-			// check the guild-level prefix
-			const guildPrefix = await prefixes.get(message.guild.id);
-			if (message.content.startsWith(guildPrefix)) prefix = guildPrefix;
-		}
-
-		// if we found a prefix, setup args; otherwise, this isn't a command
-		if (!prefix) return;
-		args = message.content.slice(prefix.length).trim().split(/\s+/);
-	} else {
-		// handle DMs
-		const slice = message.content.startsWith(globalPrefix) ? globalPrefix.length : 0;
-		args = message.content.slice(slice).split(/\s+/);
-	}
-
-	// get the first space-delimited argument after the prefix as the command
-  const command = args.shift().toLowerCase();
-  
-  if (command === 'prefix') {
-    // if there's at least one argument, set the prefix
-    if (args.length) {
-      await prefixes.set(message.guild.id, args[0]);
-      return message.channel.send(`Successfully set prefix to \`${args[0]}\``);
-    }
-  
-    return message.channel.send(`Prefix is \`${await prefixes.get(message.guild.id) || globalPrefix}\``);
-  }
-  
+// Guild join or leave
+client.on("guildCreate", (guild) => {
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}).`);
+});
+client.on("guildDelete", (guild) => {
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
 });
 
+// Ready
+client.on("ready", () => {
+  console.log(`Bot has started successfully`);
+  client.user.setActivity(`Serving fresh sources`);
+});
 
+// Commands
+client.on("message", async (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-client.login(config.token)
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command === "serve") {
+    const code = args.join("");
+    let bookData = {};
+
+    nana
+      .g(code)
+      .then((g) => {
+		bookData = g;
+		console.log(g)
+      })
+      .then((createEmbed) => {
+        const embed = new Discord.MessageEmbed(bookData)
+          .setColor("#ED2553")
+          .setAuthor(
+            bookData.scanlator,
+            "https://i.imgur.com/0A5xdvv.png",
+            "https://nhentai.net/"
+          )
+          .setThumbnail(
+            `https://t.nhentai.net/galleries/${bookData.media_id}/cover.jpg`
+          )
+          .setTitle(bookData.title.pretty)
+          .setURL(`https://nhentai.net/g/${bookData.id}`)
+          .addFields(
+		{
+            name: "Pages:",
+            value: bookData.num_pages,
+            inline: true,
+          })
+          .setTimestamp(DateTime.fromSeconds(bookData.upload_date))
+          .setFooter(
+            ` Favorites: ${bookData.num_favorites}`,
+            "https://i.imgur.com/0A5xdvv.png"
+          );
+        message.channel.send(embed);
+      })
+      .catch(function (error) {
+        throw error;
+      });
+  }
+});
+
+client.login(config.token);
