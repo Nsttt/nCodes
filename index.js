ac64fed0d935b98997debed48960c928eeafc163
Nestor <elnestinatorHD@gmail.com>
2020-10-27 22:53:17 +0100

    Added random search.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const NanaAPI = require('nana-api');
 const { DateTime } = require('luxon');
 
-const config = require('./secret/config.json');
+const config = require('./config.json');
 
 const client = new Discord.Client();
 const nana = new NanaAPI();
@@ -20,7 +20,7 @@ client.on('guildDelete', (guild) => {
 // Ready
 client.on('ready', () => {
   console.log(`Bot has started successfully`);
-  client.user.setActivity(`Serving fresh sources`);
+  client.user.setActivity(`serving fresh sources`);
 });
 
 // Commands
@@ -30,9 +30,8 @@ client.on('message', async (message) => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  if (command === 'search') {
-    message.delete()
-    const code = args.join('');
+  if (command === 's') {
+    message.delete();
     let bookData = {};
 
     let bookTags = [];
@@ -41,7 +40,7 @@ client.on('message', async (message) => {
     let bookGroup = [];
 
     nana
-      .g(code)
+      .g(command)
       .then((g) => {
         bookData = g;
       })
@@ -102,6 +101,83 @@ client.on('message', async (message) => {
           .setTimestamp(DateTime.fromSeconds(bookData.upload_date))
           .setFooter(
             ` Favorites: ${bookData.num_favorites}`,
+            'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678087-heart-512.png'
+          );
+        message.channel.send(embed);
+      });
+  }
+
+  if (command === 'r' || command === 'random') {
+    message.delete();
+    let rBookData = {};
+
+    let rBookTags = [];
+    let rBookArtist = [];
+    let rBookParody = [];
+    let rBookGroup = [];
+
+    nana
+      .random()
+      .then((g) => {
+        rBookData = g;
+      })
+      .then(() => {
+        rBookData.tags.forEach((tag) => {
+          switch (tag.type) {
+            case 'tag':
+              rBookTags.push(tag.name);
+              break;
+            case 'artist':
+              rBookArtist.push(tag.name);
+              break;
+            case 'parody':
+              rBookParody.push(tag.name);
+              break;
+            case 'group':
+              rBookGroup.push(tag.name);
+              break;
+            default:
+              null;
+          }
+        });
+      })
+      .then(() => {
+        const embed = new Discord.MessageEmbed()
+          .setColor('#ED2553')
+          .setThumbnail(
+            `https://t.nhentai.net/galleries/${rBookData.media_id}/cover.jpg`
+          )
+          .setTitle(rBookData.title.pretty)
+          .setURL(`https://nhentai.net/g/${rBookData.id}`)
+          .addFields(
+            {
+              name: 'Artist:',
+              value: rBookArtist.length ? rBookArtist.join(', ') : 'Unknown',
+              inline: true,
+            },
+            {
+              name: 'Group:',
+              value: rBookGroup.length ? rBookGroup.join(', ') : 'Unknown',
+              inline: true,
+            },
+            {
+              name: 'Parody:',
+              value: rBookParody.length ? rBookParody.join(', ') : 'Unknown',
+            }
+          )
+          .addFields(
+            {
+              name: 'Tags:',
+              value: rBookTags.length ? rBookTags.sort().join(', ') : 'None',
+            },
+            {
+              name: 'Pages:',
+              value: rBookData.num_pages,
+            }
+          )
+          .setTimestamp(DateTime.fromSeconds(rBookData.upload_date))
+          .setFooter(
+            ` Favorites: ${rBookData.num_favorites}`,
             'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678087-heart-512.png'
           );
         message.channel.send(embed);
